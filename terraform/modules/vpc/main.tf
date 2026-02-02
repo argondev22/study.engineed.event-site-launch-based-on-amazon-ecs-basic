058d0fe68f7e81135a8eb03e049282a023e917a8
argondev22 <hyoga.js@gmail.com>
2026-02-03 07:15:14 +0900

    feat(vpc): add public route table and subnet associations
    - Add public route table resource with default route to internet gateway
    - Add route table association resource to link public subnets to route table
    - Enable internet connectivity for resources in public subnets
    - Use count meta-argument to associate all public subnets dynamically

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -28,3 +28,21 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
